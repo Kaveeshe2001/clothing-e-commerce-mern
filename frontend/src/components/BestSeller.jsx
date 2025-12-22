d984faae4264fa2425fa1029314c7bebd72f3aa6
Kaveesha <kaveeshewaduge111@gmail.com>
2025-12-22 19:53:26 +0530

    BestSeller section

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect, useState } from "react"
+import ProductItem from "./ProductItem";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
+
+  useEffect(() => {
+    const bestProduct = products.filter((item) => (item.bestseller));
+    setBestSeller(bestProduct.slice(0,5));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'BEST'} text2={'SELLERS'} />
+        <p className='w-3/5 m-auto text-xs sm:text-sm ms:text-base text-gray-600'>
+            Our most-loved styles, chosen by customers for their comfort and quality.
+        </p>
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+            bestSeller.map((item, index) => (
+                <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+            ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default BestSeller
