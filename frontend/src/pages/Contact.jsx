922d591cc1a365df27e11ca15002a9c4a85bf41a
Kaveesha <kaveeshewaduge111@gmail.com>
2025-12-23 22:40:46 +0530

    Contact us

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { assets } from "../assets/assets"
+import Title from "../components/Title"
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img className="w-full md:max-w-120" src={assets.contact_img} alt="" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">No:76/1, <br /> Kandy Rd, <br /> Kiribathgoda, Sri Lanka.</p>
+          <p className="text-gray-500">Tel: 0112285197 <br /> Email: info@velvetvogue.com</p>
+          <p className="font-semibold text-xl text-gray-600">Careers at Velvet Vogue</p>
+          <p className="text-gray-500">Learn more about our teams and job openings.</p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
+        </div>
+      </div>
     </div>
   )
 }
